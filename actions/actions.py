31c7dcc032aafc1a01737e9934a670795e407da1
Jessica <jessica.becker.1995@web.de>
2021-03-17 21:52:41 +0100

    WIP connect to DB

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,13 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import sqlite3
+from sqlite3 import Error
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +28,14 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class ActionDatabaseConnection(Action):
+
+    def name(self) -> Text:
+        return "action_database_connection"
+
+    def run(self,dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text,Any]) -> List[Dict[Text, Any]]:
+
+        conn = sqlite3.connect('database\PrototypeDB.db')
